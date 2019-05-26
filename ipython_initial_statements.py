bd3f131aa5d7e811f997303172af791172d4c613
Rajat Raychaudhuri <rrc.999@gmail.com>
2019-05-26 08:25:19 -0700

    Add a startup script for jupyter console and qtconsole

--- ipython_initial_statements.py
+++ ipython_initial_statements.py
@@ -1,3 +1,23 @@
+"""
+To import all the following libraries by default everytime I start jupyter qtconsole, do the following
+(https://stackoverflow.com/questions/35446211/configuring-jupyter-default-imports)
+
+$ ipython profile create
+
+This will create the necessary folders for keeeping the startup file.
+
+$ cd ~/.ipython/profile_default/startup/
+
+In this location create the .ipy file, also read the README 
+MAKE SURE TO CREATE AN .ipy FILE SINCE IT HAS % notations
+
+Also after this there may be an initial error in the command line ipython for the matplotlib line.
+Use "jupyter console" instead
+"""
+
+import os
+import sys
+import requests as rq
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
