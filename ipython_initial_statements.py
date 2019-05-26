247c58252a52b4dbe0f2c73b0beca3e67c20d829
Rajat Raychaudhuri <rrc.999@gmail.com>
2019-05-25 22:09:50 -0700

    Updating jupyter notebook initial statements

--- ipython_initial_statements.py
+++ ipython_initial_statements.py
@@ -1,3 +1,17 @@
 import numpy as np
 import pandas as pd
-from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# excetions show up normally
+%xmode plain
+
+# plots show up inline
+%matplotlib inline
+
+
+# Jupyter notebook convert
+# jupyter nbconvert my_notebook.ipynb --to markdown --output output.md
+# jupyter nbconvert my_notebook.ipynb --to html --output output.html
+# jupyter nbconvert my_notebook.ipynb --to html --template basic --output output.html
